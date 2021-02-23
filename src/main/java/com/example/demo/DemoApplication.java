324182e0a2eba05b28f257566bbbb7272ae3f250
logesh-19mx113 <19mx113@psgtech.ac.in>
2021-02-23 18:29:36 +0530

    Updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.logging.LogLevel;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class DemoApplication {
